aaee2bd1aa04af827da730040ed7373a222cd96c
BrunaMICHELEbelo975 <brunabelo975@gmail.com>
2024-05-09 07:37:53 -0300

    terceiro commit

---
+++ javascript.js
@@ -0,0 +1,21 @@
+console.log("----A");
+const test = 50; 
+let C = 0;
+C = 10; 
+consol. log(C)
+    
+    //o vento DOM é acionado quando todo o html foi completamente carregado 
+document.addEventListener('DOMContentLoaded'), function(){
+    alert(123)
+
+     //o vento DOM é acionado quando todo o html foi completamente carregado 
+    //String: seuqencia de caracter 
+    //Number: números inteiros ou pontos flutuantes (10.4)
+    //Boolean: Retorna dois valores, true and falso
+    //Object: estrutura de dados que podem armazenar multiplos valores 
+    //Null: quando está vazio
+    //Array: objeto especial ordenado 
+    //funcion: bloco de codigo reutilizável
+    //Const: é usado para declarar uma variavél cujo valor é permanente 
+    //let: é usado para declarar variaveis que os valores podem ser retribuidos 
+}
